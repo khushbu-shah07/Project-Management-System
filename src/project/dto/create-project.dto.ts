e9281ca68cd6b1109c8571e0e14b7b91dfa95fea
khushbu-shah07 <khushbu.shah@bacancy.com>
2024-03-27 16:33:04 +0530

    Adds: project-dto

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,1 +1,16 @@
-export class CreateProjectDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+export class CreateProjectDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsDate()
+  deadline: Date;
+
+  @IsNumber()
+  pm_id: number;
+}
