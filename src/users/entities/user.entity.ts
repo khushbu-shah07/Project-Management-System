44b523c947155b52947597db2eb2b7988d83b969
Sujal Mandalia <sujal.mandalia@bacancy.com>
2024-03-27 14:20:01 +0530

    Adds:User_Table_Generation_and_database_Integration

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+enum UserRole {
+  ADMIN = 'admin',
+  EMPLOYEE = 'employee',
+  PM = 'pm'
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id:number
+
+  @Column()
+  name:string
+
+  @Column()
+  email:string
+
+  @Column()
+  password:string
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+})
+role: UserRole;
+}
