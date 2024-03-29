b42f830f07c45aad52215429dad767918691a1ba
khushbu-shah07 <khushbu.shah@bacancy.com>
2024-03-29 14:37:05 +0530

    Adds: Guard at routes

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,59 +1,124 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res, BadRequestException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  Res,
+  BadRequestException,
+  UseGuards,
+} from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { httpStatusCodes, sendResponse } from 'utils/sendresponse';
 import { Request, Response } from 'express';
+import { ProjectManagerGuard } from 'src/users/role-guard/pm.guard';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AdminGuard } from 'src/users/role-guard/admin.guard';
 
 @Controller('projects')
 export class ProjectController {
-  constructor(private readonly projectService: ProjectService) { }
+  constructor(private readonly projectService: ProjectService) {}
 
+  @UseGuards(AuthGuard, ProjectManagerGuard)
   @Post()
-  async create(@Body() createProjectDto: CreateProjectDto, @Req() req: Request, @Res() res: Response) {
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     try {
       const project = await this.projectService.create(createProjectDto);
-      return sendResponse(res, httpStatusCodes.Created, "success", "Create project", project)
+      return sendResponse(
+        res,
+        httpStatusCodes.Created,
+        'success',
+        'Create project',
+        project,
+      );
     } catch (error) {
       throw new BadRequestException(error.message);
     }
   }
 
+  @UseGuards(AuthGuard, AdminGuard)
   @Get()
   async findAll(@Req() req: Request, @Res() res: Response) {
     try {
       const projects = await this.projectService.findAll();
-      return sendResponse(res, httpStatusCodes.OK, "success", "Get All Projects", projects)
+      return sendResponse(
+        res,
+        httpStatusCodes.OK,
+        'success',
+        'Get All Projects',
+        projects,
+      );
     } catch (error) {
-      throw new BadRequestException(error.message)
+      throw new BadRequestException(error.message);
     }
   }
 
+  @UseGuards(AuthGuard, AdminGuard)
   @Get()
-  async findOne(@Param('id') id: string, @Req() req: Request, @Res() res: Response) {
+  async findOne(
+    @Param('id') id: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     try {
       const project = await this.projectService.findOne(+id);
-      return sendResponse(res, httpStatusCodes.OK, "success", "Get project by id", project)
+      return sendResponse(
+        res,
+        httpStatusCodes.OK,
+        'success',
+        'Get project by id',
+        project,
+      );
     } catch (error) {
       throw new BadRequestException(error.message);
     }
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto, @Req() req: Request, @Res() res: Response) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateProjectDto: UpdateProjectDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     try {
       await this.projectService.update(+id, updateProjectDto);
-      return sendResponse(res, httpStatusCodes.OK, "success", "Update Project", null)
+      return sendResponse(
+        res,
+        httpStatusCodes.OK,
+        'success',
+        'Update Project',
+        null,
+      );
     } catch (error) {
       throw new BadRequestException(error.message);
     }
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string, @Req() req: Request, @Res() res: Response) {
+  async remove(
+    @Param('id') id: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     try {
       await this.projectService.remove(+id);
-      return sendResponse(res, httpStatusCodes.OK, "success", "Delete project", null)
+      return sendResponse(
+        res,
+        httpStatusCodes.OK,
+        'success',
+        'Delete project',
+        null,
+      );
     } catch (error) {
       throw new BadRequestException(error.message);
     }
--- src/users/role-guard/pm.guard.ts
+++ src/users/role-guard/pm.guard.ts
@@ -1,19 +1,23 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class ProjectManagerGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    
-    const userRole = request.user?.role; 
+
+    const userRole = request.user?.role;
 
     const isProjectManager = userRole === 'pm';
-    
-     if(isProjectManager){
-        return true;
-     }
-     else{
-        throw new UnauthorizedException('only project manager has access');
-     }
+
+    if (isProjectManager) {
+      return true;
+    } else {
+      throw new UnauthorizedException('only project manager has access');
+    }
   }
 }
