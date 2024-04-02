40f801de167c426202f7b790a8f0808d58be0ae4
Parth Vasani <parth.vasani@bacancy.com>
2024-04-02 11:14:01 +0530

    Adds: entity and dto in comment module

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateCommentDto {
+    @IsNotEmpty()
+    @IsNumber()
+    task_id:number;
+
+    @IsNotEmpty()
+    @IsString()
+    content:string;
+    
+}
--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -3,5 +3,25 @@ import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateCol
 
 @Entity()
 export class Comment {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({nullable:false})
+    emp_id:number;
+
+    @Column({nullable:false})
+    task_id:number;
+
+    @Column({nullable:false})
+    content:string;
+
+    @Column({default:false})
+    edited:boolean;
+
+    @CreateDateColumn()
+    created_at:Date;
+
+    @UpdateDateColumn()
+    updated_at:Date;
 
 }
