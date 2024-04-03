672d24c54bf720e2a6d0d33ba8ce83c09ecb3c35
Parth Vasani <parth.vasani@bacancy.com>
2024-04-03 15:25:43 +0530

    Updates: comments controller and services

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -8,43 +8,43 @@ import { TaskService } from 'src/task/task.service';
 
 @Injectable()
 export class CommentsService {
-  constructor(@InjectRepository(Comment) private readonly commentRepository:Repository<Comment>, private readonly taskService:TaskService){};
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>) {}
 
-  async findAll() {
-    try{
+  async findAll():Promise<Comment[]> {
+    try {
       return this.commentRepository.find({
-        relations:['emp_id','task_id'],
-        select:{
-          emp_id:{
-            id:true,
+        relations: ['emp_id', 'task_id'],
+        select: {
+          emp_id: {
+            id: true,
           },
-          task_id:{
-            id:true,
-          }
+          task_id: {
+            id: true,
+          },
         },
-        order:{
-          created_at:'ASC',
-        }
+        order: {
+          created_at: 'ASC',
+        },
       });
-
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
 
-  async find(options){
-    try{
-      return this.commentRepository.find({where:options});
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+  async find(options):Promise<Comment[]> {
+    try {
+      return this.commentRepository.find({ where: options });
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
 
-  async findByTask(task_id:number){
-    try{
-      return this.commentRepository.find({where:{task_id:task_id} as unknown,
+  async findByEmp(emp_id: number):Promise<Comment[]> {
+    try {
+      return this.commentRepository.find({
+        where:{emp_id:{id:emp_id}} as unknown,
         relations:['task_id','emp_id'],
         select:{
           task_id:{
@@ -57,127 +57,175 @@ export class CommentsService {
         order:{
           created_at:'ASC',
         }
+        
       });
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
-  
-  async findByTaskForEmp(task_id:number,emp_id:number){
-    try{
+
+  async findByTask(task_id: number):Promise<Comment[]> {
+    try {
+      return this.commentRepository.find({
+        where: { task_id: task_id } as unknown,
+        relations: ['task_id', 'emp_id'],
+        select: {
+          task_id: {
+            id: true,
+          },
+          emp_id: {
+            id: true,
+          },
+        },
+        order: {
+          created_at: 'ASC',
+        },
+      });
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
+  }
+
+  async findByTaskForEmp(task_id: number, emp_id: number):Promise<Comment[]> {
+    try {
       const result = await this.commentRepository.find({
-        where:{
-          'task_id.id':task_id,
-          'emp_id.id':emp_id,
+        where: {
+          'task_id.id': task_id,
+          'emp_id.id': emp_id,
         } as unknown,
-        relations:['emp_id','task_id'],
-        select:{
-          emp_id:{
-            id:true,
+        relations: ['emp_id', 'task_id'],
+        select: {
+          emp_id: {
+            id: true,
           },
-          task_id:{
-            id:true,
-          }
+          task_id: {
+            id: true,
+          },
         },
-        order:{
-          created_at:'ASC',
-        }
+        order: {
+          created_at: 'ASC',
+        },
       });
-      
+
       return result;
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
 
-  async findByTaskForPM(task_id:number,pm_id:number){
-    try{
+  async findByTaskForPM(task_id: number, pm_id: number):Promise<Comment[]> {
+    try {
       const result = await this.commentRepository.find({
-        where:{
-          task_id:{
-            id:task_id,
-            project_id:{
-              pm_id:pm_id,
-            }
-          }
-        }as unknown,
-        relations:['emp_id','task_id','task_id.project_id','task_id.project_id.pm_id'],
-        select:{
-          id:true,
-          content:true,
-          edited:true,
-          created_at:true,
-          updated_at:true,
-          task_id:{
-            id:true,
-            project_id:{
-              id:false,
-            }
+        where: {
+          task_id: {
+            id: task_id,
+            project_id: {
+              pm_id: pm_id,
+            },
           },
-          emp_id:{
-            id:true,
-          }
+        } as unknown,
+        relations: [
+          'emp_id',
+          'task_id',
+          'task_id.project_id',
+          'task_id.project_id.pm_id',
+        ],
+        select: {
+          id: true,
+          content: true,
+          edited: true,
+          created_at: true,
+          updated_at: true,
+          task_id: {
+            id: true,
+            project_id: {
+              id: false,
+            },
+          },
+          emp_id: {
+            id: true,
+          },
         },
-        order:{
-          created_at:'ASC',
-        }
+        order: {
+          created_at: 'ASC',
+        },
       });
 
       return result;
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
-  
-  async create(emp_id:number,createCommentDto: CreateCommentDto) {
-    try{
-      const isTaskAssigned = await this.taskService.findTaskUser(createCommentDto.task_id,emp_id);
 
-      if(!isTaskAssigned){
-        throw new Error("This task is not assigned to you.")
-      }
+  async taskBelongsToPM(task_id: number, pm_id: number):Promise<boolean> {
+    try {
+      return await this.commentRepository.exists({
+        where: {
+          task_id: {
+            id: task_id,
+            project_id: {
+              pm_id: {
+                id: pm_id,
+              },
+            },
+          },
+        },
+        relations: [
+          'emp_id',
+          'task_id',
+          'task_id.project_id',
+          'task_id.project_id.pm_id',
+        ],
+      });
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
+  }
 
-      const comment=await this.commentRepository.create({emp_id:emp_id,...createCommentDto} as unknown as Comment);
+  async create(emp_id: number, createCommentDto: CreateCommentDto):Promise<Comment> {
+    try {
+      const comment = await this.commentRepository.create({
+        emp_id: emp_id,
+        ...createCommentDto,
+      } as unknown as Comment);
       await this.commentRepository.save(comment);
 
       return comment;
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
 
-  async update(id:number,updateCommentDto:UpdateCommentDto){
-    try{
-      const {affected} = await this.commentRepository.update({id},{edited:true,...updateCommentDto});
+  async update(id: number, updateCommentDto: UpdateCommentDto):Promise<number> {
+    try {
+      const { affected } = await this.commentRepository.update(
+        { id },
+        { edited: true, ...updateCommentDto },
+      );
       return affected;
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
 
-  async updateWithEmpId(id: number,emp_id:number, updateCommentDto: UpdateCommentDto) {
-    try{
-      const result=await this.commentRepository.update({id,emp_id:{id:emp_id}},{edited:true,...updateCommentDto});
+  async updateWithEmpId(id: number,emp_id: number,updateCommentDto: UpdateCommentDto):Promise<number> {
+    try {
+      const result = await this.commentRepository.update(
+        { id, emp_id: { id: emp_id } },
+        { edited: true, ...updateCommentDto },
+      );
       return result.affected;
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
 
-  async remove(id: number) {
-    try{
-      const {affected} = await this.commentRepository.delete({id});
+  async remove(id: number):Promise<number> {
+    try {
+      const { affected } = await this.commentRepository.delete({ id });
       return affected;
-    }
-    catch(err){
-      throw new BadRequestException(err.message)
+    } catch (err) {
+      throw new BadRequestException(err.message);
     }
   }
-
 }
