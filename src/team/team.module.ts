9dfe366a3aa631956f656ea722d303389465074d
Adarsh <adarsh.patel@bacancy.com>
2024-04-05 10:16:27 +0530

    Adds:complete functionality of notification for all required sections

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -4,9 +4,11 @@ import { TeamController } from './team.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from './entities/team.entity';
 import { TeamUser } from './entities/team-user.entity';
+import { UsersModule } from 'src/users/users.module';
+import { ProjectModule } from 'src/project/project.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team, TeamUser])],
+  imports: [TypeOrmModule.forFeature([Team, TeamUser]),UsersModule,ProjectModule],
   controllers: [TeamController],
   providers: [TeamService],
 })
