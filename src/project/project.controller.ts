448f7cc3c194b46077aed5d73d522b92a4fb3e40
Fenil <fenil.rami@bacancy.com>
2024-05-02 16:05:22 +0530

    Removes: conflicts in project controller code

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -10,10 +10,8 @@ import {
   Res,
   UseGuards,
   ForbiddenException,
-  UseInterceptors,
   HttpException,
   NotFoundException,
-  UsePipes,
 } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
@@ -24,17 +22,14 @@ import { ProjectManagerGuard } from '../../src/auth/Guards/pm.guard';
 import { AuthGuard } from '../../src/auth/Guards/auth.guard';
 import { AdminGuard } from '../../src/auth/Guards/admin.guard';
 import { AdminProjectGuard } from '../../src/auth/Guards/adminProject.guard';
-import { UserprojectService } from 'src/userproject/userproject.service';
-import { ProjectStatus } from '../notification/serviceBasedEmail/projectStatusUpdate'
-import { UsersService } from 'src/users/users.service';
 import { StartDateValidationPipe } from '../Pipes/startDatePipe';
 import { EndDateValidationPipe } from '../Pipes/endDatePipe';
 
 @Controller('projects')
 export class ProjectController {
-  constructor(private readonly projectService: ProjectService, private readonly userProject: UserprojectService, private readonly usersService: UsersService) { }
+  constructor(private readonly projectService: ProjectService) { }
 
-  @UseGuards(AuthGuard, AdminProjectGuard)
+  @UseGuards(AuthGuard, ProjectManagerGuard)
   @Post()
   async create(
     @Body(StartDateValidationPipe, EndDateValidationPipe) createProjectDto: CreateProjectDto,
@@ -44,11 +39,9 @@ export class ProjectController {
     try {
       const user = req['user'];
 
-      if(user.role === 'pm'){
       if (user.id !== createProjectDto.pm_id) {
         throw new ForbiddenException('Access Denied')
       }
-    }
       const project = await this.projectService.create(createProjectDto);
       return sendResponse(
         res,
@@ -186,50 +179,31 @@ export class ProjectController {
     @Req() req: Request,
     @Res() res: Response
   ) {
-    const project = await this.projectService.findOne(+id);
+    try {
+      const project = await this.projectService.findOne(+id);
 
-    if (!project) {
-      throw new NotFoundException('Project with given id does not exists');
-    }
-
-    if (req['user'].role === 'pm') {
-      if (req['user'].id !== project.pm_id.id) {
-        throw new ForbiddenException('Access denied to change the project status');
+      if (!project) {
+        throw new NotFoundException('Project with given id does not exists');
       }
-    }
-    
-    await this.projectService.completeProject(+id);
 
-    const pmOrAdminEmail = project.pm_id.email;
-    const projectId = project.id;
-    const projectName = project.name
-
-    const allUsersInProject = await this.userProject.getUsersFromProject(projectId)
-
-    const allUsersId = [];
-    for (const user in allUsersInProject) {
-      const userID = allUsersInProject[user].user_detail.user_id;
-      if (userID) {
-        allUsersId.push(userID)
+      if (req['user'].role === 'pm') {
+        if (req['user'].id !== project.pm_id.id) {
+          throw new ForbiddenException('Access denied to change the project status');
+        }
       }
+
+      await this.projectService.completeProject(+id);
+
+      return sendResponse(
+        res,
+        httpStatusCodes.OK,
+        'success',
+        'Complete project',
+        null
+      )
+    } catch (error) {
+      throw new HttpException(error.message, error.status || httpStatusCodes['Bad Request'])
     }
-
-    let allUsersEmail = [];
-    for (const userId in allUsersId) {
-      const usersDetail = await this.usersService.findOne(Number(allUsersId[userId]));
-      allUsersEmail.push(usersDetail.email)
-
-    }
-    console.log("all users", allUsersEmail)
-    ProjectStatus.projectStatusUpdate(pmOrAdminEmail, allUsersInProject, 'completed', projectName, this.usersService)
-
-    return sendResponse(
-      res,
-      httpStatusCodes.OK,
-      'success',
-      'Complete project',
-      allUsersInProject
-    )
   }
 
   @UseGuards(AuthGuard, AdminProjectGuard)
