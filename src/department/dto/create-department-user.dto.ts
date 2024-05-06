ce0697c805cc6ea275b01393cf92f32450e5cc25
adarsh9304 <adarsh.patel@bacancy.com>
2024-05-06 10:42:08 +0530

    Adds:swagger ApiProperty into department dto

--- src/department/dto/create-department-user.dto.ts
+++ src/department/dto/create-department-user.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsAlpha, IsNotEmpty, IsNumber } from "class-validator";
 
 export class CreateDepartmentUserDto {
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   department_id: number
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   user_id: number
 }
--- src/department/dto/create-department.dto.ts
+++ src/department/dto/create-department.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsAlpha, IsNotEmpty } from "class-validator";
 
 export class CreateDepartmentDto {
   @IsNotEmpty()
   @IsAlpha()
+  @ApiProperty()
   name: string
 }
